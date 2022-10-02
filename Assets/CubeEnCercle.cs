cb4fd45e82e0511a1c3116ee9051abdc782ff2fa
Célestin CAPTAL <celestin.captal@student.junia.com>
2022-10-02 23:10:35 +0200

    final

--- Assets/CubeEnCercle.cs
+++ Assets/CubeEnCercle.cs
@@ -4,42 +4,55 @@ using UnityEngine;
 
 public class CubeEnCercle : MonoBehaviour
 {
-
-public GameObject monCube;
-    [SerializeField] private GameObject prefabAInstantier;
+    public GameObject monCube;
+    private int TamponNbCube;
+    private float TamponRayCube;
+    private Vector3 TamponPosiCentre = new Vector3();
+    List<GameObject> IteCube = new List<GameObject>(); //important
     [SerializeField] private int nbCube;
     [SerializeField] private float rayon;
+    [SerializeField] private Vector3 position = new Vector3();
 
+    void CubeInstance()
+    {
+        TamponPosiCentre = position;
+        TamponRayCube = rayon;
+        TamponNbCube = nbCube;
+        for(int i = 0; i < nbCube; i++)
+            {
+                float angle = i * Mathf.PI * 2 / nbCube;
+                float x = Mathf.Cos(angle) * rayon;
+                float z = Mathf.Sin(angle) * rayon;
+                float degAngle = -angle * Mathf.Rad2Deg;
+                Vector3 positionCube = transform.position + new Vector3(x, 0, z);
+                Quaternion rotationCube = Quaternion.Euler(0, degAngle, 0);
+                var repCube = Instantiate(monCube, positionCube + position, rotationCube);
+                IteCube.Add(repCube);
+
+            }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < nbCube; i++)
-        {
-            float angle = i * Mathf.PI * 2 / nbCube;
-            float x = Mathf.Cos(angle) * rayon;
-            float z = Mathf.Sin(angle) * rayon;
-            float degAngle = -angle * Mathf.Rad2Deg;
-            Vector3 positionCube = transform.position + new Vector3(x, 0, z);
-            Quaternion rotationCube = Quaternion.Euler(0, degAngle, 0);
-            Instantiate(monCube, positionCube, rotationCube);
 
-        }
+        CubeInstance();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-         for(int i = 0; i < nbCube; i++)
-         {
-            float angle = i * Mathf.PI * 2 / nbCube;
-            float x = Mathf.Cos(angle) * rayon;
-            float z = Mathf.Sin(angle) * rayon;
-            float degAngle = -angle * Mathf.Rad2Deg;
-            Vector3 positionCube = transform.position + new Vector3(x, 0, z);
-            Quaternion rotationCube = Quaternion.Euler(0, degAngle, 0);
-            Instantiate(monCube, positionCube, rotationCube);
-         }
+       if(TamponNbCube != nbCube || TamponRayCube != rayon || TamponPosiCentre != position)
+       {
+        foreach (var obj in IteCube)
+        {
+            Destroy(obj);
+        }
+         CubeInstance();
+       }
 
     }
+
+
 }
